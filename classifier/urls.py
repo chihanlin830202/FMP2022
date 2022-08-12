1f8728293754d892775164bafbba40e01124ae24
chihan <chihanlin830202@gmail.com>
2022-08-12 11:56:02 +0100

    change url

--- classifier/urls.py
+++ classifier/urls.py
@@ -1,11 +1,16 @@
+from unicodedata import name
 from django.urls import path, include
 from rest_framework import routers
-
+from . import views
 from .views import ClassifierViewSet
 
 router = routers.DefaultRouter()
 router.register(r'classifier', ClassifierViewSet)
 
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
 urlpatterns = [
-    path('', include(router.urls)),
+    path('',views.ClassifierViewSet.as_view({
+        'get':'list'}),name='hello_classifier')
 ]
